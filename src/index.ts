cbd1b6c49ce29fd1a74ebc1752d102ffc7b8b654
Peter Kim <p@peter-kim.com>
2022-07-10 20:35:34 -0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 const port = Number(process.env.PORT || 8080);
 
 if (process.env.ENVIRONMENT === 'production') {
-  var redis = require('redis').createClient('6379', 'redis://red-cabrnlvh8vl8cjdfc0p0:6379');
+  var redis = require('redis').createClient('6379', 'redis://red-cabrnlvh8vl8cjdfc0p0');
 } else {
   var redis = require('redis').createClient();
 }
